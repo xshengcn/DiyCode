6b1bfbbe0e94a19b5a72c14f9f297313321cfd6e
徐升 <xshengcn@gmail.com>
2017-04-05 17:57:49 +0800

    update

--- app/src/main/java/com/xshengcn/diycode/util/HtmlUtils.java
+++ app/src/main/java/com/xshengcn/diycode/util/HtmlUtils.java
@@ -30,17 +30,7 @@ import java.util.regex.Pattern;
 public class HtmlUtils {
 
     public static String delHTMLTag(String htmlStr) {
-        String regexScript = "<script[^>]*?>[\\s\\S]*?<\\/script>"; //定义script的正则表达式
-        String regexStyle = "<style[^>]*?>[\\s\\S]*?<\\/style>"; //定义style的正则表达式
         String regexHtml = "<[^>]+>"; //定义HTML标签的正则表达式
-
-        Pattern pScript = Pattern.compile(regexScript, Pattern.CASE_INSENSITIVE);
-        Matcher mScript = pScript.matcher(htmlStr);
-        htmlStr = mScript.replaceAll(""); //过滤script标签
-
-        Pattern pStyle = Pattern.compile(regexStyle, Pattern.CASE_INSENSITIVE);
-        Matcher mStyle = pStyle.matcher(htmlStr);
-        htmlStr = mStyle.replaceAll(""); //过滤style标签
 
         Pattern pHtml = Pattern.compile(regexHtml, Pattern.CASE_INSENSITIVE);
         Matcher mHtml = pHtml.matcher(htmlStr);
