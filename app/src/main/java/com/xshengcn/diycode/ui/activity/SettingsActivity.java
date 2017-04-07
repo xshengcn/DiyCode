38068ec4fd13bc0258db9dba08feb953cf2ad73e
徐升 <xshengcn@gmail.com>
2017-04-07 16:34:32 +0800

    fix

--- app/src/main/java/com/xshengcn/diycode/ui/activity/SettingsActivity.java
+++ app/src/main/java/com/xshengcn/diycode/ui/activity/SettingsActivity.java
@@ -37,7 +37,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                 onBackPressed();
                 break;
         }
-        return super.onOptionsItemSelected(ite
+        return super.onOptionsItemSelected(item);
     }
 
     private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener =
